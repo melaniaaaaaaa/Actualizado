34007921ba7ed23ad3a66dc78054354a9c82166f
Melania <melanyracachaflores@gmail.com>
2023-03-19 23:47:22 -0500

    ProyectoEscalafon

---
+++ src/components/SidebarPlugin/SideBar.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="sidebar"
+       :style="sidebarStyle"
+       :data-color="backgroundColor"
+       :data-image="backgroundImage">
+    <div class="sidebar-wrapper">
+      <div class="logo">
+        <i class="nc-icon nc-align-center"></i>
+      </div>
+
+      <slot name="content"></slot>
+      <ul class="nav nav-main__links">
+        <!--By default vue-router adds an active class to each route link. This way the links are colored when clicked-->
+        <slot>
+          <sidebar-link v-for="(link,index) in sidebarLinks"
+                        :key="link.name + index"
+                        :to="link.path"
+                        @click="closeNavbar"
+                        :link="link">
+            <i :class="link.icon"></i>
+            <p>{{link.name}}</p>
+          </sidebar-link>
+        </slot>
+      </ul>
+      <ul class="nav nav-bottom" v-if="$slots['bottom-links']">
+        <slot name="bottom-links"></slot>
+      </ul>
+    </div>
+  </div>
+</template>
+<script>
+  import SidebarLink from './SidebarLink.vue'
+
+  export default {
+    
+    props: {
+      /*
+      title: {
+        type: String,
+        default: 'Vue LBD'
+      },*/
+      backgroundColor: {
+        type: String,
+        default: 'Gray'
+        /*,
+        validator: (value) => {
+          let acceptedValues = ['', 'blue', 'azure', 'green', 'orange', 'red', 'purple', 'black']
+          return acceptedValues.indexOf(value) !== -1
+        }*/
+      },
+      autoClose: {
+        type: Boolean,
+        default: true
+      }
+    },
+    
+  }
+
+</script>
+<style>
+
+</style>
---
+++ src/layout/TopNavbar.vue
@@ -0,0 +1,89 @@
+<template>
+  <nav class="navbar navbar-expand-lg">
+    <div class="container-fluid">
+
+      <div class="container">
+        <div class="row">
+          <div class="col-3">
+            <div class="logo-img">
+                <img src="img/unjfsc.png" alt="">
+            </div>
+          </div>
+          
+          <div class="col-9">
+            <br>
+            <a>Universidad Nacional Faustino Sanchez Carrion</a>
+            <br>
+            <a>Unidad de registros y escalafon</a>
+          </div>
+        </div>
+      </div>
+
+    
+      <button type="button"
+              class="navbar-toggler navbar-toggler-right"
+              :class="{toggled: $sidebar.showSidebar}"
+              aria-controls="navigation-index"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+              @click="toggleSidebar">
+        <span class="navbar-toggler-bar burger-lines"></span>
+        <span class="navbar-toggler-bar burger-lines"></span>
+        <span class="navbar-toggler-bar burger-lines"></span>
+      </button>
+      <div class="collapse navbar-collapse justify-content-end">
+        
+        
+        <ul class="navbar-nav ml-auto">
+          <base-dropdown tag="li">
+            <template slot="title">
+              <i class="nc-icon nc-grid-45"></i>
+              
+            </template>
+            <a class="dropdown-item" href="#">Perfil</a>
+            <a class="dropdown-item" href="#">Ayuda</a>
+            <a class="dropdown-item" href="#">Cambiar Contraseña</a>
+            <a class="dropdown-item" href="#">Cerrar Sesion</a>
+          </base-dropdown>
+        </ul>
+
+      </div>
+    </div>
+  </nav>
+</template>
+<script>
+  export default {
+    computed: {
+      routeName () {
+        const {name} = this.$route
+        return this.capitalizeFirstLetter(name)
+      }
+    },
+    data () {
+      return {
+        activeNotifications: false
+      }
+    },
+    methods: {
+      capitalizeFirstLetter (string) {
+        return string.charAt(0).toUpperCase() + string.slice(1)
+      },
+      toggleNotificationDropDown () {
+        this.activeNotifications = !this.activeNotifications
+      },
+      closeDropDown () {
+        this.activeNotifications = false
+      },
+      toggleSidebar () {
+        this.$sidebar.displaySidebar(!this.$sidebar.showSidebar)
+      },
+      hideSidebar () {
+        this.$sidebar.displaySidebar(false)
+      }
+    }
+  }
+
+</script>
+<style>
+
+</style>
